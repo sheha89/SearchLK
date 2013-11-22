4ad5231ba8512a3424dcb62f71cefd3e5d54cf43
Shehan <shehana@hsenidmobile.com>
2013-11-22 18:47:11 +0530

    Changed pakage structure

---
+++ search-lanka/src/test/java/search/lanka/core/repository/mongo/AbstractMongoRepositoryTest.java
@@ -0,0 +1,29 @@
+package search.lanka.core.repository.mongo;
+
+import org.apache.log4j.BasicConfigurator;
+import org.junit.BeforeClass;
+import search.lanka.core.config.MongoDataStore;
+
+import java.net.UnknownHostException;
+
+/**
+ *
+ */
+public class AbstractMongoRepositoryTest {
+
+    protected static MongoDataStore dataStore;
+
+    @BeforeClass
+    public static void init() {
+        BasicConfigurator.configure();
+        dataStore = new MongoDataStore();
+        dataStore.setDbName("search-test");
+        dataStore.setHost("localhost");
+        dataStore.setPort(27017);
+        try {
+            dataStore.init();
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ search-lanka/src/test/java/search/lanka/core/repository/mongo/MongoTagRepositoryTest.java
@@ -0,0 +1,39 @@
+package search.lanka.core.repository.mongo;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ */
+public class MongoTagRepositoryTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testSave() throws Exception {
+
+    }
+
+    @Test
+    public void testFindTagsById() throws Exception {
+
+    }
+
+    @Test
+    public void testFindTagsByPostId() throws Exception {
+
+    }
+
+    @Test
+    public void testFindAllTags() throws Exception {
+
+    }
+
+    @Test
+    public void testFindTagsByVendorId() throws Exception {
+
+    }
+}
